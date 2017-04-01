384d02600de46e86176d7b53ff6f0c0a66aeb747
Paige <cde0701@gmail.com>
2017-04-02 00:55:11 +0900

    included writing new posts and editing them

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
     url(r'^login/$',login, {'template_name':"auth/login.html"}, name='blog_login'),
-    url(r'^logout/$', logout, {'next_page': reverse_lazy('post_list')}, name='blog_logout')
+    url(r'^logout/$', logout, {'next_page': reverse_lazy('post_list')}, name='blog_logout'),
+
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+
 
 
 
